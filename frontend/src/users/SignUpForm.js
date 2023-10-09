229791d2c38903761234b62c4df652dd37a61da1
Wendy Wang <wendywangg928@gmail.com>
2023-10-09 15:33:53 -0400

    changed

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -8,7 +8,7 @@ function SignUpForm() {
     firstName: "",
     lastName: "",
     email: "",
-    password: "",
+    passwordDigest: "",
   });
 
   async function handleSubmit(e) {
@@ -71,8 +71,10 @@ function SignUpForm() {
             <input
               type="password"
               required
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              value={user.passwordDigest}
+              onChange={(e) =>
+                setUser({ ...user, passwordDigest: e.target.value })
+              }
               className="form-control"
               id="password"
               name="password"
